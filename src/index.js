c8af95d4d82105a720c86710badc4c55a01b553b
Valentine Dmitrieva <dimasquared@gmail.com>
2020-09-29 14:37:43 +0300

    fix: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (arguments.length && array.length !== 0) ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (arguments.length && array.length != 0) ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (arguments.length && array.length != 0) ? (array.reduce((acc, el) => acc + el) / array.length) : 0;
 }
